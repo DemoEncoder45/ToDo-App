f6446b2ba69ba65d6f62ff0545258dfb99a0fc80
Isaiah Leach <Isaiah.Leach583@gmail.com>
2024-01-11 19:24:30 -0500

    Intergrated an delete button for Item added

--- main.js
+++ main.js
@@ -7,18 +7,22 @@ let input = document.getElementById("inputField")
 addButton.addEventListener('click', function(){
     //this variable creates the paragraph element in the DOM
     const paragraph = document.createElement('p')
+    const deleteItem = document.createElement('button')
 
     //This line adds styling to the variable in question.
     paragraph.classList.add('paragraph-styling')
+    deleteItem.classList.add('deleteItem-styling')
 
     //This line will set the content of the paragraph equal to what 
     //was in the input field.
     paragraph.innerText = inputField.value
+    deleteItem.innerText = "x"
 
     //This line will appened the paragraph element that was made
     //and added as a child to the toDoContainer, in other words
     //it places whatever is in the paragraph element at the end.
     toDoContainer.appendChild(paragraph)
+    toDoContainer.appendChild(deleteItem)
 
     //This line will set the value of the input field back to empty
     //making it look like nothing is there
@@ -32,7 +36,9 @@ addButton.addEventListener('click', function(){
 
     //This line will make it so when clicked twice on the paragrah
     //text the paragraph is removed due to it being a child element
-    paragraph.addEventListener('dblclick', function(){
+
+    deleteItem.addEventListener('click', function(){
         toDoContainer.removeChild(paragraph)
+        toDoContainer.removeChild(deleteItem)
     })
 })
